a3baaca8af8bce0e69989caf5388a6127b39d3e3
cspup <yemuc@qq.com>
2023-03-12 17:41:46 +0800

    init

---
+++ src/test/java/com/csp/ehr/EhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp.ehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
